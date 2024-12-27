2e6c16f61d157814ae91d687c7d39bec44349501
Erik Dains <eadains@gmail.com>
2024-12-27 13:42:32 -0500

    Day 5 complete

--- src/day5.rs
+++ src/day5.rs
@@ -1,6 +1,16 @@
 use crate::helpers;
 use regex::Regex;
-use std::collections::HashMap;
+use std::{cmp::Ordering, collections::HashMap};
+
+fn check_rules(rules: &HashMap<&str, Vec<&str>>, line: &[&str]) -> bool {
+    line.iter().enumerate().all(|(i, &el)| match rules.get(el) {
+        // If rule exists then rest of line must NOT contain
+        // any of the values from that rule
+        Some(x) => !x.iter().any(|x| line[i + 1..].contains(x)),
+        // If no rule exists then it trivially passes
+        None => true,
+    })
+}
 
 pub fn solve() {
     let content = include_str!("../inputs/day5");
@@ -20,23 +30,33 @@ pub fn solve() {
     }
 
     let lines_re = Regex::new(r"\d+(?:,\d+)+").unwrap();
-    let lines = lines_re
+    let mut lines = lines_re
         .find_iter(content)
-        .map(|m| m.as_str().split(",").collect::<Vec<_>>());
+        .map(|m| m.as_str().split(",").collect::<Vec<_>>())
+        .collect::<Vec<_>>();
 
-    let ans1: i32 = lines
-        .filter(|line| {
-            line.iter().enumerate().all(|(i, &el)| match rules.get(el) {
-                // If rule exists then rest of line must NOT contain
-                // any of the values from that rule
-                Some(x) => !x.iter().any(|x| line[i + 1..].contains(x)),
-                // If no rule exists then it trivially passes
-                None => true,
-            })
-        })
+    let ans1 = lines
+        .iter()
+        .filter(|line| check_rules(&rules, line))
         // Have to subtract one from this because rust is zero-indexed
         .map(|line| line[line.len().div_ceil(2) - 1].parse::<i32>().unwrap())
-        .sum();
+        .sum::<i32>();
 
-    helpers::print_answers(5, ans1, "");
+    let ans2 = lines
+        .iter_mut()
+        .filter(|line| !check_rules(&rules, line))
+        .map(|line| {
+            line.sort_unstable_by(|a, b| {
+                // If b is in the rule for a then we know that b must come before a so a is greater
+                if rules.get(a).is_some_and(|rule| rule.contains(b)) {
+                    Ordering::Greater
+                } else {
+                    Ordering::Less
+                }
+            });
+            line[line.len().div_ceil(2) - 1].parse::<i32>().unwrap()
+        })
+        .sum::<i32>();
+
+    helpers::print_answers(5, ans1, ans2);
 }
